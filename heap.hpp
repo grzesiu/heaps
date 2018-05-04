e125f365491ee8289166561fc73cc3040bb19896
grzesiu <grzspychala@gmail.com>
2018-05-04 10:01:54 +0200

    Add heap parent class

--- heap.hpp
+++ heap.hpp
@@ -1,14 +1,17 @@
 #include <iostream>
 
-template<typename N>
+#include "node.hpp"
+
+template <typename N, typename T>
 class Heap
 {
-  public:
-    virtual bool empty() const = 0;
-    virtual int size() const = 0;
-    virtual N *top() const = 0;
-    virtual void *push(N *node) = 0;
-    virtual void pop() = 0;
-    virtual void increase_priority(N *node, T key) = 0;
-    virtual void erase(N *node) = 0;
+public:
+  virtual bool empty() const = 0;
+  virtual int size() const = 0;
+  virtual N *top() const = 0;
+  virtual N *push(N *node) = 0;
+  virtual N *push(T key) = 0;
+  virtual void pop() = 0;
+  virtual void increase_priority(N *node, T key) = 0;
+  virtual void erase(N *node) = 0;
 };
