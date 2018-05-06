e6a814983845a8f4b64c9f93451e32cc92c61b34
grzesiu <grzspychala@gmail.com>
2018-05-06 12:06:39 +0200

    Update heap

--- heap.hpp
+++ heap.hpp
@@ -4,15 +4,15 @@
 
 #include "node.hpp"
 
-template <typename N, typename T>
+template <typename N, typename I, typename K>
 class Heap
 {
 public:
   virtual bool empty() const = 0;
   virtual int size() const = 0;
-  virtual N *top() const = 0;
-  virtual void push(N *node) = 0;
+  virtual std::pair<I, K> top() const = 0;
+  virtual void push(I id, K key) = 0;
   virtual void pop() = 0;
-  virtual void increase_priority(N *node, T key) = 0;
-  virtual void erase(N *node) = 0;
+  virtual void increase_priority(I id, K key) = 0;
+  virtual void erase(I id) = 0;
 };
