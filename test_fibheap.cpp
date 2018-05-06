9cc66f8e3f860213e5c8e5648bedc075b92c5630
grzesiu <grzspychala@gmail.com>
2018-05-06 12:09:00 +0200

    Update fibheap test

--- test_fibheap.cpp
+++ test_fibheap.cpp
@@ -4,31 +4,24 @@
 
 int main()
 {
-    Fibheap<int> f;
-    Fibnode<int> *nodes[100];
+    Fibheap<int, int> f;
 
     for (int i = 0; i < 100; i++)
     {
-        nodes[i] = new Fibnode<int>(i);
-        f.push(nodes[i]);
+        f.push(i, i);
     }
-    std::cout << f.top()->get_key() << std::endl;
+    std::cout << f.top().second << std::endl;
 
-    f.increase_priority(nodes[50], -1);
+    f.increase_priority(50, -1);
 
-    std::cout << f.top()->get_key() << std::endl;
+    std::cout << f.top().second << std::endl;
 
     f.pop();
 
-    std::cout << f.top()->get_key() << std::endl;
+    std::cout << f.top().second << std::endl;
 
-    f.erase(nodes[99]);
-    std::cout << nodes[99]->get_key() << std::endl;
-
-    for (int i = 0; i < 100; i++)
-    {
-        delete nodes[i];
-    }
+    f.erase(99);
+    std::cout << f.size() << std::endl;
     
     return 0;
 }
