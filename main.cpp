26243370dcc2a5a2cacf2083afa5bbf231ad9063
grzesiu <grzspychala@gmail.com>
2018-05-12 19:45:23 +0200

    Add nodes with no edges to the graph

--- main.cpp
+++ main.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
-#include <map>
 #include <random>
+#include <unordered_map>
 #include <utility>
 
 #include "dijkstra.hpp"
 #include "fibheap.hpp"
 
-std::map<int, std::map<int, int>> generate_random_graph(int v, int e, int max_distance, bool undirected) {
+std::unordered_map<int, std::unordered_map<int, int>>
+generate_random_graph(int v, int e, int max_distance, bool undirected) {
     if (undirected) {
         e /= 2;
     }
-    std::map<int, std::map<int, int>> graph;
+    std::unordered_map<int, std::unordered_map<int, int>> graph;
     std::mt19937 rng;
     rng.seed(std::random_device()());
     std::uniform_int_distribution <std::mt19937::result_type> indices(0, v - 1);
     std::uniform_int_distribution <std::mt19937::result_type> distances(0, max_distance);
+
+    for (int i = 0; i < v; i++) {
+        graph[i] = std::unordered_map<int, int>();
+    }
 
     int c = 0;
     while (c < e) {
@@ -35,10 +40,11 @@ std::map<int, std::map<int, int>> generate_random_graph(int v, int e, int max_di
         }
     }
 
+
     return graph;
 }
 
-void print(const std::map<int, std::map<int, int>> &graph) {
+void print(const std::unordered_map<int, std::unordered_map<int, int>> &graph) {
     for (auto i : graph) {
         std::cout << i.first << ": ";
         for (auto j : i.second) {
@@ -52,7 +58,9 @@ int main() {
     int v = 10;
     int e = 20;
     int d = 100;
-    std::map<int, std::map<int, int>> graph = generate_random_graph(v, e, d, true);
-    Dijkstra<Fibheap, int, int> dijkstra(graph, Fibheap<int, int>(), 0, std::numeric_limits<int>::max());
+    std::unordered_map<int, std::unordered_map<int, int>> graph = generate_random_graph(v, e, d, true);
+    print(graph);
+    Dijkstra<Fibheap, int, int> dijkstra(graph, Fibheap<int, int>(), graph.begin()->first, 0,
+                                         std::numeric_limits<int>::max());
     return 0;
 }
