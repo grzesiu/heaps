bde7c9a67a2de29d81d32221695840121e6858db
grzesiu <grzspychala@gmail.com>
2018-05-14 00:59:53 +0200

    Add dijkstra header

---
+++ dijkstra.hpp
@@ -0,0 +1,13 @@
+#include <map>
+#include <memory>
+
+#include "heap.hpp"
+
+template<typename I, typename K>
+class Dijkstra {
+public:
+    Dijkstra(const std::unordered_map <I, std::unordered_map<I, K>> &graph, std::unique_ptr <Heap<I, K>> h,
+             I start, K min, K max);
+};
+
+#include "dijkstra.tpp"
